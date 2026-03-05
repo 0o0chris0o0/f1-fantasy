026b4c59f9cf8ddefbfddb12ea6f25be9945dc97
Chris Martin <chris.martin@nakedwines.com>
2026-03-05 18:17:28 +0000

    add additional build script

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -11,6 +11,12 @@ export default defineNuxtConfig({
     '@nuxt/image',
   ],
 
+  typescript: {
+    tsConfig: {
+      extends: "../../tsconfig.base.json"
+    }
+  },
+
   css: ['vue-final-modal/style.css'],
 
   icon: {
